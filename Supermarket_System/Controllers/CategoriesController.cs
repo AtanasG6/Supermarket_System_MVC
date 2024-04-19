c70f10db733d9760f7c08b9eb5e4e97013fcda5f
AtanasG6 <gyulchev666@gmail.com>
2024-04-19 18:42:24 +0300

    Add project files.

---
+++ Supermarket_System/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Supermarket_System.Models;
+
+namespace Supermarket_System.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            var category = new Category { CategoryId = id.HasValue?id.Value:0};
+
+            return View(category);
+        }
+    }
+}
