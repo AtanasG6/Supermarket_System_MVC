dc5ab7b85c899340c21aa0072844706047be5530
AtanasG6 <gyulchev666@gmail.com>
2024-04-20 15:24:22 +0300

    Post http request

--- Supermarket_System/Controllers/CategoriesController.cs
+++ Supermarket_System/Controllers/CategoriesController.cs
@@ -29,5 +29,22 @@ namespace Supermarket_System.Controllers
            
             return View(category);
         }
+
+        public IActionResult Add()
+        {
+	        return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Category category)
+        {
+	        if (ModelState.IsValid)
+	        {
+		        CategoriesRepository.AddCategory(category);
+		        return RedirectToAction(nameof(Index));
+	        }
+
+            return View(category);
+        }
     }
 }
